de1d2bbef9563ee03bc8a0eb9fb307756500c517
StefanyGo <gunawang@purdue.edu>
2017-03-20 16:59:55 -0400

    Initial Commit

---
+++ Instagram_Stef/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram_Stef
+//
+//  Created by Stefany Felicia on 20/3/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
