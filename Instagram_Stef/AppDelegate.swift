e9d9f212b43d8fb4e7da8682969bfeb91b3905d4
StefanyGo <gunawang@purdue.edu>
2017-03-20 23:32:09 -0400

    Add README

--- Instagram_Stef/AppDelegate.swift
+++ Instagram_Stef/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Instagram_Stef"
+                configuration.clientKey = "ijfpoewfkjwepofkwuiyefgwoefyugweoufyofbd"  // set to nil assuming you have not set clientKey
+                configuration.server = "https://pure-sierra-80858.herokuapp.com/"
+            })
+        )
         return true
     }
 
